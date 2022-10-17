cf067220325eb5e6afe9350191ea166bc9b81594
Evgen-iOS <jack38irk@icloud.com>
2022-10-18 01:26:48 +0800

    Initial Commit

---
+++ VideoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoList
+//
+//  Created by Evgeniy Goncharov on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
